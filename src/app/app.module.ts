4c802206f87a638bde4cb9fc31d777b302816f6c
Andre Montenegro <ajmc200@gmail.com>
2022-02-26 18:09:20 -0600

    Nav bar set up & outline of homepage

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,22 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NavBarComponent } from './components/nav-bar/nav-bar.component';
+import { HomepageComponent } from './components/homepage/homepage.component';
+import { FormsModule } from '@angular/forms';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavBarComponent,
+    HomepageComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    FormsModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/components/nav-bar/nav-bar.component.ts
@@ -0,0 +1,40 @@
+import { Component, OnInit } from '@angular/core';
+import { trigger, state, style, animate, transition, } from '@angular/animations';
+
+@Component({
+  selector: 'app-nav-bar',
+  templateUrl: './nav-bar.component.html',
+  styleUrls: ['./nav-bar.component.css'],
+  animations: [
+    trigger('openClose', [
+      state('open', style({
+        height: '180px',
+        backgroundColor: '#afaeab',
+      })),
+      state('closed', style({
+        height: '0%',
+        backgroundColor: '#afaeab'
+      })),
+      transition('open => closed', [
+        animate('0.15s')
+      ]),
+      transition('closed => open', [
+        animate('0.15s')
+      ]),
+    ]),
+  ],
+})
+export class NavBarComponent implements OnInit {
+
+  constructor() { }
+
+  isOpen = false;
+
+  ngOnInit(): void {
+  }
+
+  toggle(){
+    this.isOpen = !this.isOpen;
+  }
+
+}
