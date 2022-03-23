202adf079ff6675110dfb730bd44ccb9f31ef778
Andre Montenegro <ajmc200@gmail.com>
2022-03-22 20:39:22 -0500

    color ajustments, card formating, added content

--- src/app/components/nav-bar/nav-bar.component.ts
+++ src/app/components/nav-bar/nav-bar.component.ts
@@ -9,11 +9,11 @@ import { trigger, state, style, animate, transition, } from '@angular/animations
     trigger('openClose', [
       state('open', style({
         height: '180px',
-        backgroundColor: 'rgb(53, 53, 53)',
+        backgroundColor: '#001427',
       })),
       state('closed', style({
         height: '0%',
-        backgroundColor: 'rgb(53, 53, 53)'
+        backgroundColor: '#001427'
       })),
       transition('open => closed', [
         animate('0.15s')
