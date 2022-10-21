55eb687c38cd6464e16533a0a5446d8afdffa10b
ajmc200 <ajmc200@gmail.com>
2022-10-20 21:44:54 -0500

    Added edit/update feature
    can edit the specific room when you click on the chip

--- src/app/components/int-paint-estimate/int-paint-estimate.component.ts
+++ src/app/components/int-paint-estimate/int-paint-estimate.component.ts
@@ -38,6 +38,8 @@ export class IntPaintEstimateComponent implements OnInit {
   visible = false;
   layoutsToggle = false;
 
+  editThisRoom: any = null; //holds oldRoom to edit/update
+
   room_types: string[] = ['Bedroom', 'Bathroom', 'Kitchen', 'Living Room'];
 
   constructor(private fb: FormBuilder) { }
@@ -96,17 +98,14 @@ export class IntPaintEstimateComponent implements OnInit {
 
   add(): void {
     let roomCount = 1;
-    const roomPicked = this.roomDetailsForm.get('roomPicked')?.value;
-    let roomCountArray = this.chipCountMap.get(roomPicked);
-    
+    let roomCountArray = this.chipCountMap.get(this.roomDetailsForm.get('roomPicked')?.value);
     while(roomCountArray.has(roomCount)) roomCount++;
-
     roomCountArray.add(roomCount);
 
     this.roomz.push({
-      customName     : roomPicked+' '+roomCount, 
+      customName     : this.roomDetailsForm.get('roomPicked')?.value+' '+roomCount, 
       roomCount      : roomCount, 
-      roomPicked     : roomPicked, 
+      roomPicked     : this.roomDetailsForm.get('roomPicked')?.value, 
       sizePicked     : this.roomDetailsForm.get('sizePicked')?.value,
       conditionPicked: this.roomDetailsForm.get('conditionPicked')?.value,
       walls          : this.roomDetailsForm.controls['roomFeaturesForm'].get('walls')?.value,
@@ -128,20 +127,34 @@ export class IntPaintEstimateComponent implements OnInit {
   }
 
   edit(room: Room): void {
-    console.log(room)
+    this.editThisRoom = room;
+
+    this.roomDetailsForm.controls['roomPicked'].setValue(room.roomPicked);
+    this.roomDetailsForm.controls['sizePicked'].setValue(room.sizePicked);
+    this.roomDetailsForm.controls['conditionPicked'].setValue(room.conditionPicked);
+    this.roomDetailsForm.controls['roomFeaturesForm'].get('walls')?.setValue(room.walls);
+    this.roomDetailsForm.controls['roomFeaturesForm'].get('ceiling')?.setValue(room.ceiling);
+    this.roomDetailsForm.controls['roomFeaturesForm'].get('trim')?.setValue(room.trim);
+    this.roomDetailsForm.controls['roomFeaturesForm'].get('windows')?.setValue(room.windows);
+    this.roomDetailsForm.controls['roomFeaturesForm'].get('doors')?.setValue(room.doors);
+    this.roomDetailsForm.controls['roomFeaturesForm'].get('windowsQuantity')?.setValue(room.windowsQuantity);
+    this.roomDetailsForm.controls['roomFeaturesForm'].get('doorsQuantity')?.setValue(room.doorsQuantity);
+
+    this.visible = true;
   }
 
-  resetForm() {
-    this.roomDetailsForm.controls['roomPicked'].setValue('');
-    this.roomDetailsForm.controls['sizePicked'].setValue('');
-    this.roomDetailsForm.controls['conditionPicked'].setValue('');
-    this.roomDetailsForm.controls['roomFeaturesForm'].get('walls')?.setValue(false);
-    this.roomDetailsForm.controls['roomFeaturesForm'].get('ceiling')?.setValue(false);
-    this.roomDetailsForm.controls['roomFeaturesForm'].get('trim')?.setValue(false);
-    this.roomDetailsForm.controls['roomFeaturesForm'].get('windows')?.setValue(false);
-    this.roomDetailsForm.controls['roomFeaturesForm'].get('doors')?.setValue(false);
-    this.roomDetailsForm.controls['roomFeaturesForm'].get('windowsQuantity')?.setValue([]);
-    this.roomDetailsForm.controls['roomFeaturesForm'].get('doorsQuantity')?.setValue([]);
+  save() {
+    //remove from roomCount Set: in case they changed room type e.x. Bedroom --> Kitchen
+    const oldRoom = this.editThisRoom;
+    this.chipCountMap.get(oldRoom.roomPicked).delete(oldRoom.roomCount);
+
+    //remove room from roomz: in order to update
+    const index = this.roomz.indexOf(oldRoom);
+    if (index >= 0) this.roomz.splice(index, 1);
+
+    //"update"
+    this.submit();
+    this.editThisRoom = null;
   }
 
 }
