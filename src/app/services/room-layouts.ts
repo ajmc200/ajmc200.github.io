b5f4bc7e0f43605a6f112ab308098f78cc99e5cd
ajmc200 <ajmc200@gmail.com>
2022-10-23 21:15:58 -0500

    some style changes

--- src/app/services/room-layouts.ts
+++ src/app/services/room-layouts.ts
@@ -4,38 +4,48 @@ export const ROOMLAYOUTS: ILayouts[] = [
     {
         name: 'Bedroom',
         pictures: [
-            { picture: "../../../assets/gallery-images/before1.jpg", size: 250 },
-            { picture: "../../../assets/gallery-images/after1.jpg", size: 300 },
-            { picture: "../../../assets/gallery-images/before2.jpg", size: 350 },
-            { picture: "../../../assets/gallery-images/after2.jpg", size: 400 },
-            { picture: "../../../assets/gallery-images/before3.jpg", size: 500 },
-            { picture: "../../../assets/gallery-images/after3.jpg", size: 650 },
+            { picture: "../../../assets/gallery-images/before1.jpg", size: 100 },
+            { picture: "../../../assets/gallery-images/after1.jpg", size: 150 },
+            { picture: "../../../assets/gallery-images/before2.jpg", size: 200 },
+            { picture: "../../../assets/gallery-images/after2.jpg", size: 250 },
+            { picture: "../../../assets/gallery-images/before3.jpg", size: 300 },
+            { picture: "../../../assets/gallery-images/after3.jpg", size: 400 },
         ],
     },
 
     {
         name: 'Bathroom',
         pictures: [
-            { picture: "../../../assets/gallery-images/room1-1.jpg", size: 250 },
-            { picture: "../../../assets/gallery-images/room1-2.jpg", size: 300 },
-            { picture: "../../../assets/gallery-images/room1-3.jpg", size: 350 },
-            { picture: "../../../assets/gallery-images/room1-4.jpg", size: 450 },
-            { picture: "../../../assets/gallery-images/room1-5.jpg", size: 600 },
-            { picture: "../../../assets/gallery-images/room1-6.jpg", size: 750 },
+            { picture: "../../../assets/gallery-images/room1-1.jpg", size: 50 },
+            { picture: "../../../assets/gallery-images/room1-2.jpg", size: 100 },
+            { picture: "../../../assets/gallery-images/room1-3.jpg", size: 150 },
+            { picture: "../../../assets/gallery-images/room1-4.jpg", size: 200 },
+            { picture: "../../../assets/gallery-images/room1-5.jpg", size: 250 },
+            { picture: "../../../assets/gallery-images/room1-6.jpg", size: 300 },
         ],
     },
 
     {
         name: 'Kitchen',
         pictures: [
-            
+            { picture: "../../../assets/gallery-images/before1.jpg", size: 100 },
+            { picture: "../../../assets/gallery-images/after1.jpg", size: 150 },
+            { picture: "../../../assets/gallery-images/before2.jpg", size: 200 },
+            { picture: "../../../assets/gallery-images/after2.jpg", size: 250 },
+            { picture: "../../../assets/gallery-images/before3.jpg", size: 300 },
+            { picture: "../../../assets/gallery-images/after3.jpg", size: 400 },
         ],
     },
 
     {
         name: 'Living Room',
         pictures: [
-            
+            { picture: "../../../assets/gallery-images/room1-1.jpg", size: 50 },
+            { picture: "../../../assets/gallery-images/room1-2.jpg", size: 100 },
+            { picture: "../../../assets/gallery-images/room1-3.jpg", size: 150 },
+            { picture: "../../../assets/gallery-images/room1-4.jpg", size: 200 },
+            { picture: "../../../assets/gallery-images/room1-5.jpg", size: 250 },
+            { picture: "../../../assets/gallery-images/room1-6.jpg", size: 300 },
         ],
     }
 ]
