590e5170185b0bf5f527aa408b40af2c660b81db
ajmc200 <ajmc200@gmail.com>
2024-04-15 18:14:36 -0500

    testing api capabilities test 2.1

--- src/app/services/api.service.ts
+++ src/app/services/api.service.ts
@@ -7,14 +7,13 @@ import { Observable } from 'rxjs';
 })
 export class ApiService {
   //rootURL: String = 'http://localhost:8090/test'
-  rootURL: String =
-    'https://ec2-3-19-167-124.us-east-2.compute.amazonaws.com:8090'; // Replace with your API URL
+  rootURL: String = 'https://www.deckofcardsapi.com/api/deck/new'; // Replace with your API URL
 
   constructor(private http: HttpClient) {}
 
   // Add methods for API calls here
   getTestData(): Observable<any> {
     console.log('in service making call');
-    return this.http.get(this.rootURL + '/test', { responseType: 'text' });
+    return this.http.get(this.rootURL + '/', { responseType: 'text' });
   }
 }
